65e9016b92aa804972051c6d9210f75544b8be3e
petarpopovic914 <popovicp29@gmail.com>
2026-03-04 17:42:23 +0100

    Added Taxi game

---
+++ Taxi-Night-Shift/src/config/GameConfig.js
@@ -0,0 +1,15 @@
+export const GameConfig = {
+    startingMultiplier: 1.00,
+    growthRate: 1.0000021,
+    acceleration: 1.000008,
+    
+    tickSpeed: 60,
+    colors: {
+        background: 0x1a1a1a,
+        taxi: 0xf1c40f,
+        text: 0xffffff,
+        textShadow: 0x000000,
+        success: 0x2ecc71,
+        fail: 0xe74c3c
+    }
+};
